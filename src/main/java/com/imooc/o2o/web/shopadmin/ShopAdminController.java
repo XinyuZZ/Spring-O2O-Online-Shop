003140d204095dfa503c712d6e8668e901cb2eab
zhuxinyu <zhuxinyu@pcitech.com>
2022-08-09 23:19:20 +0800

    created test int

--- src/main/java/com/imooc/o2o/web/shopadmin/ShopAdminController.java
+++ src/main/java/com/imooc/o2o/web/shopadmin/ShopAdminController.java
@@ -5,10 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping(value = "/shopadmin",method = RequestMethod.GET)
+@RequestMapping(value = "/shopadmin", method = RequestMethod.GET)
 public class ShopAdminController {
     @RequestMapping("/shopoperation")
-    public String shopOperation(){
-            return "shop/shopoperation";
+    public String shopOperation() {
+        int a = 1;
+        return "shop/shopoperation";
+
     }
 }
